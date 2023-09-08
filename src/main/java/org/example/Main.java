23b2fdb18e2f90c9597e30543aba2e32e34dcd0d
Harishdevposnarmetta <harishnarmeta1@gmail.com>
2023-09-08 12:00:29 +0530

    Data added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
+        System.out.printf("Hello and welcome! This is my first project");
 
         // System.out.printf("This is my third statement Build Success");
 
